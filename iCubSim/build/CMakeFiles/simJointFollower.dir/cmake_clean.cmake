2e43b23a7c488b7d7721b8e3576f6197351bd661
Tarik Kelestemur <tkelestemur@gmail.com>
2016-05-20 17:00:52 +0300

    git repo created

---
+++ iCubSim/build/CMakeFiles/simJointFollower.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/simJointFollower.dir/simJointFollower.cpp.o"
+  "simJointFollower.pdb"
+  "simJointFollower"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/simJointFollower.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/joint_transformer.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+#include <ros/ros.h>
+#include <tf/transform_listener.h>
+#include "geometry_msgs/Pose.h"
+
+using namespace std;
+
+int main(int argc, char** argv){
+  ros::init(argc, argv, "joint_transformer");
+  ros::NodeHandle node;
+
+  string target_frame = "/joint_11";
+  string ref_frame = "/joint_8";
+
+  geometry_msgs::Pose jointPose;
+
+
+  tf::TransformListener listener;
+
+  ros::Rate rate(10.0);
+  while (node.ok()){
+    tf::StampedTransform transform;
+    try{
+      listener.lookupTransform(target_frame, ref_frame, ros::Time(0), transform);
+    }
+    catch (tf::TransformException &ex) {
+      ROS_ERROR("%s",ex.what());
+      ros::Duration(1.0).sleep();
+      continue;
+    }
+
+    jointPose.position.x = transform.getOrigin().x();
+    jointPose.position.y = transform.getOrigin().y();
+    jointPose.position.z = transform.getOrigin().z();
+    jointPose.orientation.x = transform.getRotation().x();
+    jointPose.orientation.y = transform.getRotation().y();
+    jointPose.orientation.z = transform.getRotation().z();
+    jointPose.orientation.w = transform.getRotation().w();
+
+    ROS_INFO("x =[%f], y =[%f], z =[%f]", jointPose.position.x, jointPose.position.y, jointPose.position.z);
+    ROS_INFO("X =[%f], Y =[%f], Z =[%f], W =[%f]", jointPose.orientation.x, jointPose.orientation.y, jointPose.orientation.z, jointPose.orientation.w);
+
+
+    rate.sleep();
+  }
+  return 0;
+};
